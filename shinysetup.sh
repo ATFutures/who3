f06222932b8b4442859796110cae4282151c254f
Robin Lovelace <rob00x@gmail.com>
2019-05-28 16:05:41 +0100

    Add example app

--- shinysetup.sh
+++ shinysetup.sh
@@ -1,19 +1,29 @@
 
 
+# docker run --rm -d 3838:3838 \ 
 docker run --rm -p 3838:3838 \
     -v /mnt/57982e2a-2874-4246-a6fe-115c199bc6bd/atfutures/shinyapps/:/srv/shiny-server/ \
     -v /mnt/57982e2a-2874-4246-a6fe-115c199bc6bd/atfutures/shinylog/:/var/log/shiny-server/ \
     rocker/shiny
 
 sudo chown $USER ../shinyapps
+sudo chown $USER ../shinylogs
 
 wget https://github.com/rstudio/shiny-examples/archive/master.zip
 unzip master.zip
 ls shiny-examples-master/001*  
+ls shiny-examples-master/ | grep map
 
 ls -hal ../shinyapps
 mv -v shiny-examples-master/001*  ../shinyapps
 mv -v shiny-examples-master/002*  ../shinyapps
 mv -v shiny-examples-master/003*  ../shinyapps
 
+mkdir ../shinyapps/att
+cp exampleLeafletApp.R ../shinyapps/att/app.R
+
 # next step: container admin with docker attach
+docker exec -it competent_yonath /bin/bash
+
+R
+install.packages("leaflet")
