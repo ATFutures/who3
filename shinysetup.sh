10088d8514e03317dbe377e0af307cc5bf0ed7d9
Robin Lovelace <rob00x@gmail.com>
2019-05-29 07:25:51 +0100

    Add docker setup test code

--- shinysetup.sh
+++ shinysetup.sh
@@ -2,9 +2,10 @@
 
 # docker run --rm -d 3838:3838 \ 
 docker run --rm -p 3838:3838 \
+    -e PASSWORD=<YORPASS> \
     -v /mnt/57982e2a-2874-4246-a6fe-115c199bc6bd/atfutures/shinyapps/:/srv/shiny-server/ \
     -v /mnt/57982e2a-2874-4246-a6fe-115c199bc6bd/atfutures/shinylog/:/var/log/shiny-server/ \
-    rocker/shiny
+    atfutures/pct:v0.0.1
 
 sudo chown $USER ../shinyapps
 sudo chown $USER ../shinylogs
@@ -35,4 +36,10 @@ q()
 # stop shiny-server
 exit
 
-docker restart competent_yonath
+# see https://github.com/rocker-org/rocker/issues/235
+docker build pct -t atfutures/pct:v0.0.1
+
+cp -Rv ~/hd/npct/pct-shiny/ ../shinyapps/
+
+# mkdir -p ../shinyapps/pct-outputs-regional-R/commute/msoa/
+# cp -Rv ~/hd/npct/pct-outputs-regional-R/ ../shinyapps/
