4ac932b47cf08d0f775df62b98b27844fde059f0
Robin Lovelace <rob00x@gmail.com>
2019-09-02 19:58:46 +0100

    Add london code

--- code/london.R
+++ code/london.R
@@ -1,1 +1,20 @@
 # Aim estimate active transport potential in London
+
+# From https://github.com/moveability/calibration
+
+settlement_name = "london"
+
+library (osmdata)
+
+bb <- getbb (settlement_name, format_out = "polygon") # no polygon available
+hw <- opq (bb) %>%
+  add_osm_feature (key = "highway") %>%
+  osmdata_sc (quiet = FALSE)
+hw <- osm_elevation (hw, elev_file = elev_file)
+saveRDS (hw, file = file.path (data_dir, "accra-hw.Rds"))
+
+library (moveability)
+attractors <- get_attractors (bb)
+saveRDS (attractors, file = file.path (data_dir, "accra-attractors.Rds"))
+green_polys <- get_green_space (bb)
+saveRDS (green_polys, file = file.path (data_dir, "accra-green.Rds"))
