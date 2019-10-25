978b391f2d035b045aa7341e81f4fc417ed77740
Robin Lovelace <rob00x@gmail.com>
2019-10-25 16:11:55 +0100

    Add new R scripts for scenario dev

---
+++ code/mode-split-cities.R
@@ -0,0 +1,5 @@
+# Aim: get mode split for cities
+
+d1 = htmltab::htmltab("https://en.wikipedia.org/wiki/Modal_share", which = 1)
+d2 = htmltab::htmltab("https://en.wikipedia.org/wiki/Modal_share", which = 2)
+
