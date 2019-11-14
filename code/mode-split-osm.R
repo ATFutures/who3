cbf700e823778e4ce635d0f5554d54ff8a022830
Robin Lovelace <rob00x@gmail.com>
2019-11-14 22:18:45 +0000

    Update mode split osm code

--- code/mode-split-osm.R
+++ code/mode-split-osm.R
@@ -5,10 +5,22 @@ cities
 # get vital (but small) transport data ------------------------------------
 
 library(osmdata)
-bus_stops_accra = opq("accra") %>% add_osm_feature("highway", "bus_stop") %>% osmdata_sf()
-city_bbs = lapply(1:nrow(cities), FUN = function(x) try(getbb(x, format_out = "sf_polygon")))
-redo_bb = sapply(city_bbs, class) == 
+accra_point = cities %>% filter(City == "Accra")
+bus_stops_accra = opq(sf::st_bbox(accra_point$bb_poly)) %>% add_osm_feature("highway", "bus_stop") %>% osmdata_sf()
+get_bus_stops = function(shp) {
+  opq(sf::st_bbox(shp)) %>% add_osm_feature("highway", "bus_stop") %>% osmdata_sf()
+  message("got bus stops!")
+}
+get_cycleways = function(shp) {
+  opq(sf::st_bbox(shp)) %>% add_osm_feature("highway", "cycleway") %>% osmdata_sf()
+  message("got bus stops!")
+}
 
+# nominatim died, reverting to bbs
+# city_bbs = lapply(1:nrow(cities), FUN = function(x) try(getbb(x, format_out = "sf_polygon")))
+
+bus_stops_cities = lapply(cities$bb_poly, FUN = get_bus_stops)
+cycleways_cities = lapply(cities$bb_poly, FUN = get_cycleways)
 
 # download global osm data ------------------------------------------------
 
@@ -26,8 +38,9 @@ summary(cities)
 
 # out-takes ---------------------------------------------------------------
 
-
-for(i in 1:5) {
+devtools::install_github("ropensci/osmdata")
+library(osmdata)
+for(i in 1:10) {
   tryCatch(expr = {
     bus_stops_accra = opq("accra") %>% add_osm_feature("highway", "bus_stop") %>% osmdata_sf()
   },
